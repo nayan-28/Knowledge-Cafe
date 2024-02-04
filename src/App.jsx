bdde62b23e72747fa294e103468ff9c3e11f0ea3
nayan-28 <nayanmalakar720@gmail.com>
2024-02-05 00:50:18 +0600

    add json data

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./Components/Header/Header";
+import Blogs from "./Components/Blogs/Blogs";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
-        <h1 className="text-4xl bg-red-300">Knowledge Cafe</h1>
+        <Header></Header>
+        <Blogs></Blogs>
       </div>
     </>
   );
